9b0e6c8dc1a8ed58f3fd5eecdacbe8e0865b3337
Mohamed chiheb ben jemaa <med.chiheb@live.com>
2018-09-10 14:44:01 +0100

    create controllers

--- src/main/java/com/enable/restfullapp/helloworld/HelloWorldController.java
+++ src/main/java/com/enable/restfullapp/helloworld/HelloWorldController.java
@@ -1,13 +1,11 @@
 package com.enable.restfullapp.helloworld;
 
-import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,3 +1,3 @@
 insert into user values (1001,sysdate(),'Med chiheb');
-insert into user values (1002,sysdate(),'Rahma');
+insert into user values (1002,sysdate(),'Hey there');
 insert into post values (500,'item description',1001);
